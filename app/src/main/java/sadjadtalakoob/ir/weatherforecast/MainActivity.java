07d76d59a4d6033ffd17b575849a59c8850d5dcb
Sadjad <sadjadtalakob74@gmail.com>
2021-07-08 09:43:38 +0400

    Adding some element ui

--- app/src/main/java/sadjadtalakoob/ir/weatherforecast/MainActivity.java
+++ app/src/main/java/sadjadtalakoob/ir/weatherforecast/MainActivity.java
@@ -3,12 +3,26 @@ package sadjadtalakoob.ir.weatherforecast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txt_view);
+        editText = findViewById(R.id.edt_text);
+        listView = findViewById(R.id.list_view);
+
+
+
+
     }
 }
